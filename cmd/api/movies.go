c7cbea72a87234ec50e9932ab11cc3bc0049c26b
mukun_poudel <mukun@herm.is>
2023-07-18 08:44:08 +0530

    page 86

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -8,12 +8,6 @@ import (
 	"greenlight.millansharma.net/internal/data"
 )
 
-// Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
-// return a plain-text placeholder response.
-func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
-}
-
 // Add a showMovieHandler for the "GET /v1/movies/:id" endpoint. For now, we retrieve
 // the interpolated "id" parameter from the current URL and include it in a placeholder
 // response.
@@ -41,4 +35,31 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		// Use the new serverErrorResponse() helper.
 		app.serverErrorResponse(w, r, err)
 	}
+}
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	// Declare an anonymous struct to hold the information that we expect to be in the
+	// HTTP request body (note that the field names and types in the struct are a subset
+	// of the Movie struct that we created earlier). This struct will be our *target
+	// decode destination*.
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+	// Initialize a new json.Decoder instance which reads from the request body, and
+	// then use the Decode() method to decode the body contents into the input struct.
+	// Importantly, notice that when we call Decode() we pass a *pointer* to the input
+	// struct as the target decode destination. If there was an error during decoding,
+	// we also use our generic errorResponse() helper to send the client a 400 Bad
+	// Request response containing the error message.
+	// err := json.NewDecoder(r.Body).Decode(&input)
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+	// Dump the contents of the input struct in a HTTP response.
+	fmt.Fprintf(w, "%+v\n", input)
 }
